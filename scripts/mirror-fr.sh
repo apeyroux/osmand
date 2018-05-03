518bbf06d9a83e0d18dc8f6364533977d648c1fe
Alexandre Peyroux <alex@xn--wxa.email>
2018-05-03 19:12:25 +0200

    add mirror-fr script

---
+++ scripts/mirror-fr.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+osmand -f france \
+       -f french \
+       -f world \
+       -f Guyana \
+       -f Spain \
+       -f gb_england \
+       -f guiana \
+       -f italy \
+       -f germany \
+       -f belgium \
+       -f switzerland \
+       -f luxembourg \
+       -f martinique \
+       -f guadeloupe \
+       -f oceania \
+       -f reunion \
+       -f saint-pierre \
+       -f mayotte \
+       -d $1
