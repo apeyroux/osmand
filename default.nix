77bcd5d9a2344d9aaf3959939d875bea1fb0168a
Alexandre Peyroux <alex@xn--wxa.email>
2018-05-03 14:32:49 +0200

    travis build with nixpkgs=master@git

--- default.nix
+++ default.nix
@@ -1,3 +1,3 @@
-# (import <nixpkgs> {}).haskellPackages.developPackage { root = ./.; }
+(import <nixpkgs> {}).haskellPackages.developPackage { root = ./.; } # nix-build -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/master.tar.gz
 # { pkgs ? import /home/alex/src/nixpkgs {} }:
-(import /home/alex/src/nixpkgs {}).haskell.packages.ghc841.developPackage { root = ./.; }
+# (import /home/alex/src/nixpkgs {}).haskell.packages.ghc841.developPackage { root = ./.; }
